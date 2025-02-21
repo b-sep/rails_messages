7cd8cc0c99cab81812a159e3a28f415b1c9b8c32
Júnior <b.sep@live.com>
2025-02-20 23:05:16 -0300

    remove chat model

--- app/models/chat.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Chat < ApplicationRecord
-  belongs_to :initiator, class_name: 'User'
-  belongs_to :recipient, class_name: 'User'
-  has_many :messages, dependent: :destroy
-
-  scope :all_chats_for, ->(user_id) { where(initiator_id: user_id).or(where(recipient_id: user_id)) }
-end
